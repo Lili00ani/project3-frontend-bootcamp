0d20dfd1986d4745ac406c32eaae367fcc02a733
kendigm <Kendiigm@gmail.com>
2024-03-29 09:33:41 +0500

    Authentication Sidebar Profile  Screen Design.

--- src/pages/userPages/myProfilePage.js
+++ src/pages/userPages/myProfilePage.js
@@ -1,7 +1,105 @@
 import React from "react";
+import Avatar from "@mui/material/Avatar";
+import Typography from "@mui/material/Typography";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import MessageIcon from "@mui/icons-material/Message";
+import CalendarTodayIcon from "@mui/icons-material/CalendarToday";
+import BookmarkIcon from "@mui/icons-material/Bookmark";
+import ContactSupportIcon from "@mui/icons-material/ContactSupport";
+import SettingsIcon from "@mui/icons-material/Settings";
+import HelpIcon from "@mui/icons-material/Help";
+import ExitToAppIcon from "@mui/icons-material/ExitToApp";
+import { Link } from "react-router-dom";
 
-const myProfilePage = () => {
-  return <div>myProfilePage myProfilePage</div>;
-};
+export default function myProfilePage() {
+  //
+  const user = {
+    name: "Glory Kendi",
+    profileImg: "/logo192.png",
+    unreadMessages: 3, // Number of unread messages
+  };
 
-export default myProfilePage;
+  return (
+    <div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          marginBottom: 20,
+        }}
+      >
+        <div
+          style={{
+            padding: "1rem 3rem",
+          }}
+        >
+          <Avatar src={user.profileImg} alt="Profile Image" />
+        </div>
+        <Typography variant="body1" style={{ marginLeft: 10 }}>
+          {user.name}
+        </Typography>
+      </div>
+      <List>
+        <ListItem button>
+          <ListItemIcon>
+            <CalendarTodayIcon />
+          </ListItemIcon>
+          <ListItemText primary="My Profile" />
+        </ListItem>{" "}
+        <ListItem button>
+          <ListItemIcon>
+            <MessageIcon />
+          </ListItemIcon>
+          <ListItemText primary="Message" />
+          {user.unreadMessages > 0 && (
+            <Typography variant="body2" color="primary">
+              {user.unreadMessages}
+            </Typography>
+          )}
+        </ListItem>
+        <ListItem button>
+          <ListItemIcon>
+            <CalendarTodayIcon />
+          </ListItemIcon>
+          <ListItemText primary="Calendar" />
+        </ListItem>
+        <ListItem button>
+          <ListItemIcon>
+            <BookmarkIcon />
+          </ListItemIcon>
+          <ListItemText primary="Bookmark" />
+        </ListItem>
+        <ListItem button>
+          <ListItemIcon>
+            <ContactSupportIcon />
+          </ListItemIcon>
+          <ListItemText primary="Contact Us" />
+        </ListItem>
+        <ListItem button>
+          <ListItemIcon>
+            <SettingsIcon />
+          </ListItemIcon>
+          <ListItemText primary="Settings" />
+        </ListItem>
+        <ListItem button>
+          <ListItemIcon>
+            <HelpIcon />
+          </ListItemIcon>
+          <ListItemText primary="Help & FAQs" />
+        </ListItem>
+        <ListItem button component={Link} to="/signin">
+          <ListItemIcon>
+            <ExitToAppIcon />
+          </ListItemIcon>
+          <ListItemText primary="Sign Out" />
+        </ListItem>
+        <ListItem button>
+          <ListItemText primary="Language" />
+        </ListItem>
+      </List>
+    </div>
+  );
+}
