cf7e2445624128a0908e32fc73a144dc329c8c1c
Lili00ani <saputri.liliani@gmail.com>
2024-04-07 10:33:44 +0800

    updated checkout page ui

--- src/pages/userPages/eventBookingPage.js
+++ src/pages/userPages/eventBookingPage.js
@@ -9,15 +9,18 @@ import Button from "@mui/material/Button";
 import Select from "@mui/material/Select";
 import axios from "axios";
 import { Box } from "@mui/material";
+import Typography from "@mui/material/Typography";
+import { ThemeProvider } from "@mui/material/styles";
 
 //-----------Components-----------//
 import { BACKEND_URL } from "../../constant.js";
+import theme from "../../theme";
 
 export default function EventBookingPage({ eventId, isFree }) {
-  const [quantity, setQuantity] = useState("");
-  const [capacity, setCapacity] = useState("");
+  const [quantity, setQuantity] = useState(1);
+  const [capacity, setCapacity] = useState();
+  const [event, setEvent] = useState();
 
-  //will add condition if price is free, don't need to go through stripe payment checkout
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -34,16 +37,61 @@ export default function EventBookingPage({ eventId, isFree }) {
     fetchData();
   }, [eventId]);
 
-  const handleQuantityChange = (event) => {
-    setQuantity(event.target.value);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`${BACKEND_URL}/events/${eventId}`);
+        setEvent(response.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, [eventId]);
+
+  // const handleQuantityChange = (event) => {
+  //   setQuantity(event.target.value);
+  // };
+
+  // const arrayTickets = Array.from({ length: capacity }, (_, i) => i + 1);
+
+  const handleIncrement = () => {
+    if (quantity < capacity) {
+      setQuantity(quantity + 1);
+    }
   };
 
-  const arrayTickets = Array.from({ length: capacity }, (_, i) => i + 1);
+  const handleDecrement = () => {
+    if (quantity > 1) {
+      setQuantity(quantity - 1);
+    }
+  };
+
+  const totalPrice = event && event.price * quantity;
+  const displayPrice = totalPrice === 0 ? "Free" : `$ ${totalPrice}`;
 
   return (
-    <div>
-      <Box sx={{ margin: "30px" }}>
-        <FormControl sx={{ m: 1, minWidth: 120 }}>
+    <ThemeProvider theme={theme}>
+      <Box
+        sx={{
+          margin: "30px",
+          width: "70vw",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Typography variant="caption" paragraph>
+          Number of Tickets
+        </Typography>
+        <Box
+          sx={{
+            justifyContent: "center",
+            display: "flex",
+          }}
+        >
+          {/* <FormControl sx={{ m: 1, minWidth: 120 }}>
           <InputLabel id="quantity-tickets">Tickets</InputLabel>
           <Select
             labelId=""
@@ -59,19 +107,42 @@ export default function EventBookingPage({ eventId, isFree }) {
             ))}
           </Select>
           <FormHelperText>Quantity</FormHelperText>
-        </FormControl>
-      </Box>
-      <Box sx={{ margin: "30px", display: "flex", justifyContent: "center" }}>
-        <Button
-          component={Link}
-          to={isFree ? "/free-return" : "/checkout"}
-          state={{ eventId: eventId, quantity: quantity }}
-          variant="contained"
-          color="primary"
+        </FormControl> */}
+
+          <Button onClick={handleDecrement}>-</Button>
+          <Typography variant="h3" sx={{ margin: "5vw" }}>
+            {quantity}
+          </Typography>
+          <Button onClick={handleIncrement}>+</Button>
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
         >
-          Checkout
-        </Button>
+          <Typography variant="caption" paragraph>
+            Total
+          </Typography>
+          <Typography variant="h5" gutterBottom>
+            {displayPrice}
+          </Typography>
+        </Box>
+
+        <Box sx={{ margin: "30px", display: "flex", justifyContent: "center" }}>
+          <Button
+            component={Link}
+            to={isFree ? "/free-return" : "/checkout"}
+            state={{ eventId: eventId, quantity: quantity }}
+            variant="contained"
+            color="primary"
+          >
+            Checkout
+          </Button>
+        </Box>
       </Box>
-    </div>
+    </ThemeProvider>
   );
 }
